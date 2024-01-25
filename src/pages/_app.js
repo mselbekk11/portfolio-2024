656b45d68c13e91577eec7fe9eaddf815e462014
mselbekk11 <mselbekk11@gmail.com>
2024-01-24 18:50:27 -0800

    speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
   );
 }
