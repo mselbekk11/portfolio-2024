90fd54dcdcd6182221469a74c8bc834a608ff45a
mselbekk11 <mselbekk11@gmail.com>
2024-03-26 19:53:38 -0700

    simple analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,13 @@ import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
+      <Script src='https://scripts.simpleanalyticscdn.com/latest.js' />
       <Analytics />
       <SpeedInsights />
     </ThemeProvider>
